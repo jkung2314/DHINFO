4de9bbae1ef499ff73f47bee6d9bae9ac5a28305
Jonathan Kung <jhkung@ucsc.edu>
2018-03-03 12:21:04 -0800

    Added main program

---
+++ dh.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
+from twilio import twiml
+import requests
+
+app = Flask(__name__)
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms():
+
+    message_body = request.form['Body'].lower()
+
+    cowell = 'http://nutrition.sa.ucsc.edu/menuSamp.asp?locationNum=05&locationName=Cowell&sName=&naFlag=', 'Cowell/Stevenson Dining Hall'
+    ten = 'http://nutrition.sa.ucsc.edu/menuSamp.asp?locationNum=40&locationName=College+Nine+%26+Ten&sName=&naFlag=', 'Colleges Nine & Ten Dining Hall'
+    merill = 'http://nutrition.sa.ucsc.edu/menuSamp.asp?locationNum=20&locationName=Crown+Merrill&sName=&naFlag=', 'Merill Cowell'
+    porter = 'http://nutrition.sa.ucsc.edu/menuSamp.asp?locationNum=25&locationName=Porter&sName=&naFlag=', 'Porter College'
+    rcc = 'http://nutrition.sa.ucsc.edu/menuSamp.asp?locationNum=30&locationName=Rachel+Carson+Oakes+Dining+Hall&sName=&naFlag=', 'Rachel Carson College'
+
+    found = 'Found in '
+    array = [cowell,ten,merill,porter,rcc]
+    page = requests.get(cowell[0])
+    foundFlag = False
+    for college in array:
+        page = requests.get(college[0])
+        if message_body in page.content.lower():
+            message = (college[1] + ", ")
+            found = found + message
+            foundFlag = True
+    if foundFlag == False:
+        found = "Item not found."
+
+    resp = MessagingResponse()
+    #resp.message("You said: {}".format(found))
+    resp.message(found)
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
